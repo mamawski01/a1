f81499bb57dcd07c589ef445237df70582004158
mamawski01 <caretib123@gmail.com>
2024-06-07 13:23:44 +0800

    ok

--- be/routes/controller/getusers.js
+++
@@ -1,3 +0,0 @@
-export function getusers() {
-  return;
-}
--- be/routes/userRoutes.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { getusers } from "./controller/getusers";
-
-const router = express.Router();
-
-router.get("/users", getusers);
